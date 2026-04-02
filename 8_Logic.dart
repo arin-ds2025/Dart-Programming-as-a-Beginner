c10baaea8aaf47e51ab4c41f167dbf42cbafcf77
Arin Ahmed <arinwebdev2024@gmail.com>
2026-04-02 13:48:16 +0600

    Basics of logic

---
+++ 8_Logic.dart
@@ -0,0 +1,11 @@
+main(){
+  var roll = 758578;
+
+  if(roll==758572 && (roll>758571 || roll<758573)) print("Hey,Arin! How's your day?");
+  else if(roll == 758578 && (roll>758577 || roll<758579)) print("Hey,Nayem! How's your day?");
+  else if(roll == 758571  && (roll>758570 || roll<758572)) print("Hey,Murad! How's your day?");
+  else{
+    print("Bruh, you're not in the database!");
+  }
+  print('Sorry for the confusion in Logic..! LoL');
+}
